3ad8223812dfe28f05878da2fb9719a03583ab57
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-10-15 11:30:14 -0500

    Moved some things to different files

---
+++ styles.py
@@ -0,0 +1,24 @@
+# Estilo del banner de título de la página
+CONTENT_STYLE = {
+    "margin-left": "auto",
+    "margin-right": "auto",
+    "padding-top": "1rem",
+    "background-color": "#FBD600"
+}
+
+# argumentos de estilo para la sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "10rem",
+    "padding": "2rem 1rem",
+    "background-color": "#AFADA6",  # estos son los códigos de colores HTML
+}
+
+CONTENT_STYLE_SIDEBAR = {
+    "margin-left": "10rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
