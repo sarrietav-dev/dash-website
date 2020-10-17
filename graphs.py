61a006a92d1348438af95b4ce2e2d7fa373f24fd
JPDavison <47907953+JPDavison@users.noreply.github.com>
2020-10-17 12:58:35 -0500

    Add files via upload
    Ajuste en fuente de datos de las gráficas 1-5, se quito graf0, y el slider

---
+++ graphs.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+######################################################################## CARGA DE BASES DE DATOS:
+bd_agr_month = pd.read_csv("data/offcorss_agr_tienda_año_mes.csv",
+                 sep=";")
+
+bd_agr_year = pd.read_csv("data/offcorss_agregada_año.csv",
+                 sep=";")
+
+#########################################################################TRANSFORMACION DE BASES DE DATOS
+
+bd_agr_month["ticket_prom"] = bd_agr_month["vlr_neto"] / bd_agr_month["qt_facturas_unq"]
+bd_agr_month["year"] = bd_agr_month["year_factura"].str[:4]
+bd_agr_month["trim_año"] = bd_agr_month["year"].astype(str) + "-Q" + bd_agr_month["trimestre"].astype(str)
+
+# GRAFICA 1: BAR Ingresos por canal
+graf1 = px.bar(bd_agr_month, x="year_factura", y="vlr_neto", color="tipo_tienda", width=600, height=400,
+               color_discrete_map={
+                   "TIENDA PROPIA": "gold",
+                   "TIENDA VIRTUAL": "black",
+                   "FRANQUICIAS": "silver"
+               },
+               category_orders={"tipo_tienda": [
+                   "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
+               title="Ingresos por canal (Millones COP)")
+graf1.update_layout(xaxis_tickangle=90)
+
+# GRAFICA 2: BAR Número de ventas por canal
+graf2 = px.bar(bd_agr_month, x="year_factura", y="qt_facturas_unq",
+               color="tipo_tienda", width=800, height=400,
+               color_discrete_map={
+                   "TIENDA PROPIA": "gold",
+                   "TIENDA VIRTUAL": "black",
+                   "FRANQUICIAS": "silver"
+               },
+               category_orders={"tipo_tienda": [
+                   "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
+               title="Número de ventas por canal")
+graf2.update_layout(xaxis_tickangle=90)
+
+
+# GRAFICA 3: PIE Share de ingresos
+graf3 = px.pie(bd_agr_month, values='vlr_neto', names='tipo_tienda', color="tipo_tienda",  width=400, height=400,
+               color_discrete_map={
+                   "TIENDA PROPIA": "gold",
+                   "TIENDA VIRTUAL": "black",
+                   "FRANQUICIAS": "silver"
+               },
+               title='%Ingresos por canal')
+
+# GRAFICA 4:PIE Share de número de ventas
+graf4 = px.pie(bd_agr_month, values='qt_facturas_unq', names='tipo_tienda', color="tipo_tienda",  width=400, height=400,
+               color_discrete_map={
+                   "TIENDA PROPIA": "gold",
+                   "TIENDA VIRTUAL": "black",
+                   "FRANQUICIAS": "silver"
+               },
+               title='%Número de ventas por canal')
+
+# GRAFICA 5: LINE Evolución ticket promedio
+graf5 = px.line(bd_agr_month, x="year_factura", y="ticket_prom",
+                color="tipo_tienda", width=800, height=400,
+                color_discrete_map={
+                    "TIENDA PROPIA": "gold",
+                    "TIENDA VIRTUAL": "black",
+                    "FRANQUICIAS": "silver"
+                },
+                category_orders={"tipo_tienda": [
+                    "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
+                title="Evolución ticket promedio")
+graf5.update_layout(xaxis_tickangle=90)
--- page.py
+++ page.py
@@ -2,170 +2,21 @@ import os
 import dash
 import random
 import pandas as pd
-import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import plotly.graph_objects as go
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+from graphs import *
+from styles import *
+from layouts import main_page, sidebar
+
+app = dash.Dash(external_stylesheets=[
+                dbc.themes.LUX], suppress_callback_exceptions=True)
 
 
-##################################################################### DATA ORIGINAL
-bd = pd.read_csv("data/offcorss_transac_agr_202010102240.csv",
-                 sep = ";")
-
-#################################################### AGRUPACION 1
-
-bd_grupo1 = bd[["year_mes_factura","trimestre", "year_factura", "tipo_tienda",  "vlr_neto", "qt_facturas_unq", "qt_articulo_unq"]].\
-groupby(["year_mes_factura", "trimestre", "year_factura", "tipo_tienda"]).sum() 
-
-bd_grupo1 = bd_grupo1.reset_index()
-bd_grupo1["vlr_neto_M"] = bd_grupo1["vlr_neto"] / 1000000 # Manejar la cifra en millones?
-bd_grupo1["ticket_prom"] = bd_grupo1["vlr_neto"] / bd_grupo1["qt_facturas_unq"]
-
-################################################### PROCEDIMIENTO 1 +  GRAFICA 0
-# % Crecimiento MoM
-
-para = ["TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]
-for t in para:
-    deltas = []
-    temp = bd_grupo1[bd_grupo1["tipo_tienda"] == t]["vlr_neto"].reset_index()
-    for i, e in enumerate(temp["vlr_neto"]):
-        meses_ex = [0,1,2,3,4,5,6,7,8,9,10,11]
-        if i in meses_ex:
-            a = None
-        else:
-            a = float((e/temp["vlr_neto"][i-12])-1)
-        deltas.append(a)
-    deltas = pd.Series(deltas)
-    if t == "TIENDA PROPIA":        
-        deltas_todo =   deltas
-    else:
-        deltas_todo =  pd.concat([deltas_todo, deltas], axis = 1)
-deltas_todo = pd.concat([pd.Series(bd_grupo1["year_mes_factura"].unique()), deltas_todo], axis = 1)
-deltas_todo = deltas_todo.iloc[12:,]
-deltas_todo.columns = ["fecha", "TP", "TV", "FR"]
-################################## GRAFICA 0: Ingresos month over mont MoM
-
-graf0 = go.Figure(layout =
-go.Layout(
-    autosize=False,
-    width=800,
-    height=400,
-title='Variaciones MoM por canal')
-
-                  )
-graf0.add_trace(go.Scatter(x=deltas_todo["fecha"], y=deltas_todo["TP"],
-                    mode='lines+markers',
-                    name='TP'))
-graf0.add_trace(go.Scatter(x=deltas_todo["fecha"], y=deltas_todo["TV"],
-                    mode='lines+markers',
-                    name='TV'))
-graf0.add_trace(go.Scatter(x=deltas_todo["fecha"], y=deltas_todo["FR"],
-                    mode='lines+markers',
-                    name='FR'))
-
-
-######################### GRAFICA 1: BAR Ingresos por canal
-graf1 = px.bar(bd_grupo1, x = "year_mes_factura", y = "vlr_neto_M", color = "tipo_tienda", width=800, height=400,
-             color_discrete_map={
-                "TIENDA PROPIA": "gold",
-                "TIENDA VIRTUAL": "black",
-                "FRANQUICIAS": "silver"
-             },
-             category_orders={"tipo_tienda": ["TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
-            title = "Ingresos por canal (Millones COP)")
-graf1.update_layout(xaxis_tickangle=90)
-
-###################### GRAFICA 2: BAR Númeor de ventas por canal
-graf2 = px.bar(bd_grupo1, x = "year_mes_factura", y = "qt_facturas_unq", 
-            color = "tipo_tienda", width=800, height=400,
-             color_discrete_map={
-                "TIENDA PROPIA": "gold",
-                "TIENDA VIRTUAL": "black",
-                "FRANQUICIAS": "silver"
-             },
-             category_orders={"tipo_tienda": ["TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},             
-            title = "Número de ventas por canal")
-graf2.update_layout(xaxis_tickangle=90)
-
-
-###################### GRAFICA 3: PIE Share de ingresos
-graf3 = px.pie(bd_grupo1, values='vlr_neto_M', names='tipo_tienda', color = "tipo_tienda",  width=400, height=400,
-                color_discrete_map={
-                "TIENDA PROPIA": "gold",
-                "TIENDA VIRTUAL": "black",
-                "FRANQUICIAS": "silver"
-             },               
-             title='%Ingresos por canal')
-
-
-###################### GRAFICA 4:PIE Share de número de ventas
-graf4 = px.pie(bd_grupo1, values='qt_facturas_unq', names='tipo_tienda', color = "tipo_tienda",  width=400, height=400,
-                color_discrete_map={
-                "TIENDA PROPIA": "gold",
-                "TIENDA VIRTUAL": "black",
-                "FRANQUICIAS": "silver"
-             },             
-             title='%Número de ventas por canal')
-
-
-
-###################### GRAFICA 5: LINE Evolución ticket promedio
-graf5 = px.line(bd_grupo1, x = "year_mes_factura", y = "ticket_prom", 
-            color = "tipo_tienda", width=800, height=400,
-             color_discrete_map={
-                "TIENDA PROPIA": "gold",
-                "TIENDA VIRTUAL": "black",
-                "FRANQUICIAS": "silver"
-             },
-             category_orders={"tipo_tienda": ["TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},             
-            title = "Evolución ticket promedio")
-graf5.update_layout(xaxis_tickangle=90)
-
-############################################### Components
-
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-navbar = html.Div(
-    [
-        dbc.Row(
-            dbc.Col(
-                html.Img(src=app.get_asset_url("ddg.png"))
-            )
-        ),
-        dbc.Row(
-            dbc.Col(
-                dbc.Nav(
-                    [
-                        dbc.NavLink("Page 1"),
-                        dbc.NavLink("Page 2"),
-                        dbc.NavLink("Page 3")
-                    ],
-                    vertical=True,
-                    pills=True
-                )
-            )
-        )
-    ],
-    style={
-        "position": "fixed",
-        "top": 0,
-        "left": 0,
-        "bottom": 0,
-        "witdh": "16rem",
-        "padding": "2rem 3rem",
-        "color": "white",
-        "background-color": "black"
-    }
-)
-
-CONTENT_STYLE = {
-    "margin-left": "11rem",
-    "margin-right": "auto",
-    "padding-top": "10rem",
-    "background-color": "#FBD600"
-}
-
+# Esto es data de ejemplo para colocar en el mapa:
 df = pd.DataFrame({
     "Name": ["Example" + str(i + 1) for i in range(100)],
     # Latitude between 4.700100 and 4.710000
@@ -184,38 +35,122 @@ map_graph = dcc.Graph(
     figure=fig
 )
 
+# Aca van todas la gráficas:
 graphs = html.Div([
     dbc.Row([
         dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    map_graph
-                ])
-            ])
+            dcc.Graph(id="graf1", figure=graf1)
+        ]),
+        dbc.Col([
+            dcc.Graph(id="graf3", figure=graf3)
         ])
     ]),
     dbc.Row([
         dbc.Col([
-            # One bar graph
+            dcc.Graph(id="graf5", figure=graf5)
         ]),
-        dbc.Col([
-            # One line raph
-        ])
-    ]),
-
-    dcc.Graph(id = "graf1", figure = graf1),
-    dcc.Graph(id = "graf3", figure = graf3),
-    dcc.Graph(id = "graf5", figure = graf5),
-    dcc.Graph(id = "graf0", figure = graf0),
+        #dbc.Col([
+            #dcc.Graph(id="graf0", figure=graf0)
+        #])
+    ])
 ])
 
+# Este row es una prueba
+row = html.Div(
+    [dbc.Row(dbc.Col(html.Div("Gráficos de análisis exploratorio")))
+     ], style={})
+
+# Dropdown1:
+# Este dropdown es para graf1 y graf3, para seleccionar ver en cantidad o rev
+
+dropdown1 = html.Div([
+    dbc.DropdownMenu(
+        label="Ingresos o productos",
+        children=[
+            dbc.DropdownMenuItem("Ingresos"),
+            dbc.DropdownMenuItem(divider=True),
+            dbc.DropdownMenuItem("Productos")
+        ])
+])
+
+##slider = dcc.Slider(id="slider",
+##                    min=bd["year_factura"].min(),
+##                    max=bd["year_factura"].max(),
+##                    value=bd["year_factura"].max(),
+##                    marks={str(year): str(year) for year in bd["year_factura"].unique()}, step=None
+##                    )
+
+#################################################################################################################################
+############################################################## CONTENIDO #########################################################
 content = html.Div([
-    html.Div(style=CONTENT_STYLE), graphs
-], style={})
+    html.H1(["Offcorss Dash mock-up"], style=CONTENT_STYLE),
+    row,
+    dropdown1,
+    #slider,
+    graphs
+], style={"margin-left": "10rem"})
 
-app.layout = html.Div([navbar, content])
+
+app.layout = html.Div([
+    dcc.Location(id="url"),  # refresh = False
+    sidebar(),
+    html.Div(id="page-content")
+])
+
+hoja_principal = html.Div([
+    dbc.Row(dbc.Col(sidebar)), dbc.Row(dbc.Col(main_page(app)))
+])
+
+hoja_1_layout = html.Div([
+    sidebar(), content,
+    html.Div(id='page-1-content')
+])
+
+hoja_2_layout = html.Div([
+    html.Div(id='page-2-content'),
+    html.H1("Hoja 2 prueba"),
+    sidebar()
+])
 
 
+################################################################################################################################
+####################################################### INTERACTIVIDAD #########################################################
+@app.callback(
+    [Output(f"link_hoja_{i}", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
+)
+def habilitar_link(pathname):
+    if pathname == "/":
+        return True, False, False
+    return [pathname == f"/hoja-{i}" for i in range(1, 4)]
+
+
+@app.callback(
+    Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/hoja-1":
+        return hoja_1_layout
+    elif pathname == "/hoja-2":
+        return hoja_2_layout
+    elif pathname == "/main":
+        return hoja_principal
+
+
+@app.callback(Output("graf1", "figure"), Input("slider", "value"))
+def change_graphs(year_value):
+
+    df = bd_grupo1[bd_grupo1["year_factura"] == year_value]
+    graf1_a = px.bar(df, x="mes_factura", y="vlr_neto_M", color="tipo_tienda", width=600, height=400,
+                   color_discrete_map={
+                       "TIENDA PROPIA": "gold",
+                       "TIENDA VIRTUAL": "black",
+                       "FRANQUICIAS": "silver"
+                   },
+                   category_orders={"tipo_tienda": [
+                       "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
+                   title="Ingresos por canal (Millones COP)")
+    graf1_a.update_layout(xaxis_tickangle=90)
+    return graf1_a
 
 
 if __name__ == "__main__":
