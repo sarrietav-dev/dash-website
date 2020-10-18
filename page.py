8d33783118591bba82e2a7fd2e849e45619aff33
JPDavison <47907953+JPDavison@users.noreply.github.com>
2020-10-18 14:39:10 -0500

    Adición de summary y gráfica 0
    Se agregan estadísticas en forma  de texto y tabla en el objeto summary.
    Se agrega la gráfica 0, con su respectiva transfomración de dataframe necesaria.

--- page.py
+++ page.py
@@ -7,6 +7,7 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
+import numpy as np
 
 from graphs import *
 from styles import *
@@ -14,6 +15,8 @@ from layouts import main_page, sidebar
 
 app = dash.Dash(external_stylesheets=[
                 dbc.themes.LUX], suppress_callback_exceptions=True)
+
+
 # Esto es data de ejemplo para colocar en el mapa:
 df = pd.DataFrame({
     "Name": ["Example" + str(i + 1) for i in range(100)],
@@ -40,24 +43,18 @@ graphs = html.Div([
             dcc.Graph(id="graf1", figure=graf1)
         ]),
         dbc.Col([
-            dcc.Graph(id="graf3", figure=graf3)
+            dcc.Graph(id="graf3", figure=graf3 ,style={"margin-left":"10rem"}), 
         ])
     ]),
     dbc.Row([
         dbc.Col([
-            dcc.Graph(id="graf5", figure=graf5)
-        ]),
-
-        dbc.Col([
             dcc.Graph(id="graf0", figure=graf0)
+        ]),
+        dbc.Col([
+            dcc.Graph(id="graf5", figure=graf5)
         ])
     ])
 ])
-
-# Este row es una prueba
-row = html.Div(
-    [dbc.Row(dbc.Col(html.Div("Gráficos de análisis exploratorio")))
-     ], style={})
 
 # Dropdown1:
 # Este dropdown es para graf1 y graf3, para seleccionar ver en cantidad o rev
@@ -79,43 +76,66 @@ dropdown1 = html.Div([
 ##                    marks={str(year): str(year) for year in bd["year_factura"].unique()}, step=None
 ##                    )
 
-
 #################################################################################################################################
 ############################################################## CONTENIDO #########################################################
+
+row = html.Div(
+    [dbc.Row(dbc.Col(html.H5("Resumen de la base:")))
+     ], style={})
+
+
+tabla1 = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in bd_agr_year.iloc[:,:-2].columns],
+    data=bd_agr_year.iloc[:,:-2].to_dict('records')
+    )
+
+summary = html.Div([
+    dbc.Row([
+        dbc.Col([
+            html.Div("Núm registros:" + '{:10,.0f}'.format(bd_unicos.iloc[0,0]), style = {"margin-left":"2rem"}),
+            html.Div("Clientes únicos:" + '{:10,.0f}'.format(bd_unicos.iloc[0,2]), style = {"margin-left":"2rem"}),
+            html.Div("Compras únicas:" + '{:10,.0f}'.format(bd_unicos.iloc[0,1]), style = {"margin-left":"2rem"}),
+            html.Div("Total Revenue:" + '{:10,.0f}'.format(bd_unicos.iloc[0,4]), style = {"margin-left":"2rem"}),
+                   
+        ]),
+        dbc.Col([
+            html.Div("Info desde:" + '{}'.format(bd_unicos.iloc[0,5]), style = {"margin-right":"10rem"}),
+            html.Div("Info. hasta" + '{}'.format(bd_unicos.iloc[0,6]), style = {})
+        ]),
+    ]),
+])
+
+
 content = html.Div([
     html.H1(["Offcorss Dash mock-up"], style=CONTENT_STYLE),
-    row,
+    row, 
+    summary,
+    tabla1,
     dropdown1,
-    slider,
     graphs
 ], style={"margin-left": "10rem"})
 
-hoja_principal = html.Div([
-    html.Div(dbc.Row(dbc.Col(sidebar(False))), style={"display": "none"}),
-    
-    dbc.Row(dbc.Col(main_page(app, True)))
-])
 
 app.layout = html.Div([
-    hoja_principal,
     dcc.Location(id="url"),  # refresh = False
-], id="page-content")
+    sidebar(),
+    html.Div(id="page-content")
+])
+
+hoja_principal = html.Div([
+    dbc.Row(dbc.Col(sidebar)), dbc.Row(dbc.Col(main_page(app)))
+])
 
 hoja_1_layout = html.Div([
-    sidebar(True), content, main_page(app, False),
+    sidebar(), content,
     html.Div(id='page-1-content')
 ])
 
 hoja_2_layout = html.Div([
     html.Div(id='page-2-content'),
     html.H1("Hoja 2 prueba"),
-    main_page(app, False),
-    sidebar(True)
-])
-
-hoja_2_layout = html.Div([
-    html.Div(id='page-2-content'),
-    html.H1("Hoja 2 prueba"),
+    html.H1(str(np.array(bd_unicos.iloc[:,1])), style = {"margin-left": "5rem"}),   
     sidebar()
 ])
 
@@ -156,57 +176,6 @@ def change_graphs(year_value):
                    category_orders={"tipo_tienda": [
                        "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
                    title="Ingresos por canal (Millones COP)")
-    graf1_a.update_layout(xaxis_tickangle=90)
-    return graf1_a
-
-################################################################################################################################
-####################################################### INTERACTIVIDAD #########################################################
-
-@app.callback(
-    [Output(f"link_hoja_{i}", "active") for i in range(1, 4)],
-    [Input("url", "pathname")],
-)
-def habilitar_link(pathname):
-    if pathname == "/":
-        return True, False, False
-    return [pathname == f"/hoja-{i}" for i in range(1, 4)]
-
-
-@app.callback(
-    Output("page-content", "children"),
-    [
-        Input("link-hoja-main", "n_clicks"),
-        Input("link-hoja-1", "n_clicks"),
-        Input("link-hoja-2", "n_clicks"),
-        Input("link-hoja-3", "n_clicks"),
-        Input("button-kpi", "n_clicks"),
-        Input("button-cluster", "n_clicks"),
-        Input("button-result", "n_clicks"),
-        Input("button-xxi", "n_clicks"),
-    ]
-)
-def display_page(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8):
-    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-    if "link-hoja-1" in changed_id or "button-kpi" in changed_id:
-        return hoja_1_layout
-    elif "link-hoja2" in changed_id or "button-cluster" in changed_id:
-        return hoja_2_layout
-    else:
-        return hoja_principal
-
-# @app.callback(Output("graf1", "figure"), Input("slider", "value"))
-def change_graphs(year_value):
-
-    df = bd_grupo1[bd_grupo1["year_factura"] == year_value]
-    graf1_a = px.bar(df, x="mes_factura", y="vlr_neto_M", color="tipo_tienda", width=600, height=400,
-                     color_discrete_map={
-                         "TIENDA PROPIA": "gold",
-                         "TIENDA VIRTUAL": "black",
-                         "FRANQUICIAS": "silver"
-                     },
-                     category_orders={"tipo_tienda": [
-                         "TIENDA PROPIA", "TIENDA VIRTUAL", "FRANQUICIAS"]},
-                     title="Ingresos por canal (Millones COP)")
     graf1_a.update_layout(xaxis_tickangle=90)
     return graf1_a
 
